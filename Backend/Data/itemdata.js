76a89a22dade2cf0f076a39122305e7f5a95044c
Rahul Kumar <rahulrp8677@gmail.com>
2025-06-24 23:59:34 +0530

    List of Data

---
+++ Backend/Data/itemdata.js
@@ -0,0 +1,25 @@
+const Data=[
+    {
+        name: "Bananas",
+        price: 60
+    },
+    {
+        name: "Smartphone",
+        price: 25000
+    },
+    {
+        name: "Headphones",
+        price: 1500
+    },
+    {
+        name: "Milk",
+        price: 50
+    },
+    {
+        name: "Backpack",
+        price: 1200
+    }
+]
+
+
+module.exports= Data;
